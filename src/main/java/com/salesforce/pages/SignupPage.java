c394be3c077629407588ac6a6abcbd0098f73c4e
Dinesh <dineshreddy.jetti@gmail.com>
2022-12-01 23:20:27 +0800

    test

--- src/main/java/com/salesforce/pages/SignupPage.java
+++ src/main/java/com/salesforce/pages/SignupPage.java
@@ -10,30 +10,7 @@ import cucumber.api.java.en.When;
 public class SignupPage extends ProjectSpecificMethods{
 	
 	
-	@Given("User enter the Firstname as {string}")
-	public  SignupPage user_enter_the_Firstname_as(String data) {
-		
-		clearAndType(locateElement(Locators.ID, "firstName"), data);
-		reportStep(data+" entered successfully","pass");
-		return this;
-	}
-
 	
-	@Given("User click on the checkbox")
-	public SignupPage user_click_on_the_checkbox() {
-	   click(locateElement(Locators.XPATH, "//input[@type='checkbox']"));
-	   reportStep("Show password checkbox clicked successfully","pass");
-	   return this;
-	}
-
-	@Given("User click on the Next button")
-	public MobileverificationPage user_click_on_the_Next_button() {
-		click(locateElement(Locators.XPATH, "//span[text()='Next']"));
-		reportStep("Next Button clicked successfully","pass");
-		return new MobileverificationPage();
-	}
-
-
 
 	@When("User enters the FirstName as {string}")
 	public SignupPage user_enters_the_FirstName_as(String data) {
